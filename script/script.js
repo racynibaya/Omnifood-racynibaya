37a1dd0c9f2f288e735380cb96124d45f49c4c38
Racyn Ibaya <racynibaya@outlook.com>
2022-10-12 17:20:52 -0400

    adding the preventDefault() to specific link only

--- script/script.js
+++ script/script.js
@@ -12,17 +12,18 @@ const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach(function (link) {
   link.addEventListener('click', function (e) {
-    e.preventDefault();
-
     const href = link.getAttribute('href');
 
-    if (href === '#')
+    if (href === '#') {
+      e.preventDefault();
       window.scrollTo({
         top: 0,
         behavior: 'smooth',
       });
+    }
 
     if (href !== '#' && href.startsWith('#')) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
 
       sectionEl.scrollIntoView({ behavior: 'smooth' });
